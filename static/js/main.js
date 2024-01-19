10635cd9a69ddd03ea8c225c02264b5c21a61020
steward <stewardhere@icloud.com>
2024-01-19 14:17:40 +0100

    Email obfuscation due to SPAM

--- static/js/main.js
+++ static/js/main.js
@@ -23,3 +23,47 @@ Object.keys(classConfig).forEach(className => {
   elements.forEach((el) => observer.observe(el));
 });
 
+
+var btns = document.querySelectorAll(".modal-button");
+var spans = document.querySelectorAll(".close");
+
+btns.forEach(btn => {
+  btn.onclick = function() {
+    var modal = document.getElementById(this.dataset.modal);
+    modal.style.display = "flex";
+    window.getComputedStyle(modal).opacity;
+    modal.style.opacity = 1;
+  }
+});
+
+spans.forEach(span => {
+  span.onclick = function() {
+    var modal = this.parentElement.parentElement;
+    modal.style.opacity = 0;
+    setTimeout(function() {
+      modal.style.display = "none";
+    }, 500);
+  }
+});
+
+window.onclick = function(event) {
+  if (event.target.classList.contains('modal')) {
+    var modal = event.target;
+    modal.style.opacity = 0;
+    setTimeout(function() {
+      modal.style.display = "none";
+    }, 500);
+  }
+};
+
+function openEmail() {
+  var emailAddress = "histeward@proton.me";
+  var subject = "Get in touch!"; // You can customize the email subject
+  var body = "Hi Steward!";       // You can customize the email body
+
+  var mailtoLink = 'mailto:' + encodeURIComponent(emailAddress) +
+    '?subject=' + encodeURIComponent(subject) +
+    '&body=' + encodeURIComponent(body);
+
+  window.location.href = mailtoLink;
+}
