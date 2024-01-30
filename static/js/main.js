6a17652d3fd33529eca685b8402ce5723d436258
steward <stewardhere@icloud.com>
2024-01-30 21:02:51 +0100

    revamped to neumorphism

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+// Fade in when in viewport
 const classConfig = {
   "fade-hidden": "fade-show",
   "fade-left-hidden": "fade-left-show",
@@ -24,6 +25,8 @@ Object.keys(classConfig).forEach(className => {
 });
 
 
+
+// Modal function
 var btns = document.querySelectorAll(".modal-button");
 var spans = document.querySelectorAll(".close");
 
@@ -56,14 +59,100 @@ window.onclick = function(event) {
   }
 };
 
+// Email obfuscation
 function openEmail() {
-  var emailAddress = "histeward@proton.me";
-  var subject = "Get in touch!"; // You can customize the email subject
-  var body = "Hi Steward!";       // You can customize the email body
+  var emailAddress = "welcome@histeward.com";
+  var subject = "I want to get in touch!";
+  var body = "Hi Steward!";
 
   var mailtoLink = 'mailto:' + encodeURIComponent(emailAddress) +
     '?subject=' + encodeURIComponent(subject) +
     '&body=' + encodeURIComponent(body);
 
   window.location.href = mailtoLink;
+}
+
+function toggleNav() {
+  const navContainer = document.querySelector(".nav-container");
+  const navItems = document.querySelectorAll(".nav-item");
+
+  navItems.forEach((nav) => {
+    nav.classList.toggle("open");
+  });
+  navContainer.classList.toggle("open");
+}
+
+function toggleTheme() {
+  const htmlElement = document.documentElement;
+  const currentTheme = htmlElement.getAttribute('data-theme');
+
+  const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+  htmlElement.setAttribute('data-theme', newTheme);
+}
+
+function checkThemePreference() {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const htmlElement = document.documentElement;
+
+  if (prefersDarkMode) {
+    htmlElement.setAttribute('data-theme', "dark");
+  } else {
+    htmlElement.setAttribute('data-theme', "light");
+  }
+}
+
+checkThemePreference()
+
+
+// mp3 function
+const mp3FileUrl = 'static/other/hero_audio_file.mp3';
+
+const audio = new Audio(mp3FileUrl);
+const playPauseButton = document.getElementById('playPause');
+const progressBarFill = document.querySelector('.progress-bar-fill');
+
+audio.addEventListener('timeupdate', updateProgressBar);
+audio.addEventListener('ended', handleAudioEnded);
+
+function updatePlayPauseButtonState() {
+  playPauseButton.classList.toggle('playing');
+}
+
+function resetSong() {
+  audio.currentTime = 0;
+  if (audio.paused) {
+    audio.play();
+    playPauseButton.innerHTML = '<i class="fas fa-pause"></i>';
+  }
+}
+
+function togglePlayPause() {
+  if (audio.paused) {
+    audio.play();
+    playPauseButton.innerHTML = '<i class="fas fa-pause"></i>';
+    console.log("playing supposedly")
+  } else {
+    audio.pause();
+    playPauseButton.innerHTML = '<i class="fas fa-play"></i>';
+    console.log("pausing supposedly")
+  }
+  updatePlayPauseButtonState();
+}
+
+
+function updateProgressBar() {
+  const percentage = (audio.currentTime / audio.duration) * 100;
+  progressBarFill.style.width = `${percentage}%`;
+}
+
+function handleAudioEnded() {
+  updatePlayPauseButtonState();
+}
+
+let mybutton = document.getElementById("scrollUp");
+
+function goHome() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
